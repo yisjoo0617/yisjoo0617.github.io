815becfedc18dae774357ac461dd65348d3de895
seonkyung <seonk@tessa.art>
2024-06-30 20:23:31 +0900

    feat: 리스트 추가

---
+++ src/config/const.ts
@@ -0,0 +1,6 @@
+export const baseDomain = 'https://yisjoo0617.github.io';
+
+export const blogName = '개발 블로그';
+export const blogDesc = '개발 지식과 경험을 공유합니다.';
+
+export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
